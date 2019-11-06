4be093514113ba703ec07c7210d10c0b89a8adf2
linux_china <libing.chen@gmail.com>
2019-11-06 11:43:36 -0800

    NativeGreetingResourceTest

---
+++ src/test/java/org/mvnsearch/quickstart/NativeGreetingResourceTest.java
@@ -0,0 +1,9 @@
+package org.mvnsearch.quickstart;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceTest extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
